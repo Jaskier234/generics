39a266fc068c0539d643d41ab49393f5dc0958b4
jaskier234 <jaskier234@gmail.com>
2022-06-28 17:43:25 +0200

    Cleanup in Main

--- app/Main.hs
+++ app/Main.hs
@@ -4,22 +4,8 @@ module Main where
 import Generics
 import Quotes
 import Test
+import Example
 import Language.Haskell.TH
 
-data A = A Int String [Int] | B Int | C String
-
--- $(evalEverywhere 'simpl [t| Prog |] [t| Expr |])
-
 main :: IO ()
--- main = runQ (generateFunc 'id ''A) >>= putStrLn . pprint -- putStrLn fun
-main = testMain -- do 
---     putStrLn "Before transformation"
---     print prog
---     putStrLn "After transformation"
---     print $ simplProg3 prog
--- main = putStrLn $ $(printToExp =<< execEverywhere 'succ [t| Prog |] [t| Int |])
--- main = print $(do 
---     mainType <- [t| Prog |]
---     transformType <- [t| Expr |]
---     mainQ mainType transformType)
-
+main = exampleMain
